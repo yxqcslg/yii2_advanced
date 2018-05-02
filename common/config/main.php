f081ed66fc383aa9a739852a5ccdbbec494ea28c
yuxiaoqing <yuxiaoqing@jingdigital.com>
2018-05-02 15:19:52 +0800

    rcbc

--- common/config/main.php
+++ common/config/main.php
@@ -9,5 +9,12 @@ return [
         'cache' => [
             'class' => 'yii\caching\FileCache',
         ],
+        'authManager' => [
+            'class' => 'yii\rbac\DbManager',
+            'itemTable' => 'auth_item',
+            'assignmentTable' => 'auth_assignment',
+            'itemChildTable' => 'auth_item_child',
+            'ruleTable'=>'auth_rule',
+        ]
     ],
 ];

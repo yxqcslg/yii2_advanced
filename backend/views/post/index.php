1f193dacdb9afbd6fd849311e88c03ae972683cf
yuxiaoqing <yuxiaoqing@jingdigital.com>
2018-05-03 15:16:22 +0800

    search

--- backend/views/post/index.php
+++ backend/views/post/index.php
@@ -13,7 +13,7 @@ $this->params['breadcrumbs'][] = $this->title;
 <div class="post-index">
 
     <h1><?= Html::encode($this->title) ?></h1>
-    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
+    <?php  echo $this->render('_search', ['model' => $searchModel]); ?>
 
     <p>
         <?= Html::a('Create Post', ['create'], ['class' => 'btn btn-success']) ?>
@@ -30,9 +30,9 @@ $this->params['breadcrumbs'][] = $this->title;
                 'contentOptions'=>['width'=>'30px'],
             ],
             'title',
-            //'author_id',
             [
-                'attribute'=>'author_id',
+                'attribute'=>'authorName',
+                'label'=>'作者',
                 'value'=>'author.nickname',
             ],
             //'content:ntext',
